f5fcb4d21c24358ec817645a6d626b14cfddc994
Lyubov9302 <lyubovyurynets@gmail.com>
2025-09-13 11:29:20 +0300

    ++

--- src/components/GridItem/GridItem.tsx
+++ src/components/GridItem/GridItem.tsx
@@ -1,5 +1,8 @@
 import style from "./GridItem.module.css";
+interface GridItemProps {
+  children: React.ReactNode;
+}
 
-export default function GridItem({ children }) {
+export default function GridItem({ children }: GridItemProps) {
   return <li className={style.item}>{children}</li>;
 }
